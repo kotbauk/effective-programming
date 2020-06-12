31cdd159545f69079943b4fdad258d2489f33990
Zykin Mikhail <kotbauk@gmail.com>
2020-06-12 18:55:45 +0700

    Init lab3

--- lab3/src/lab3.c
+++ lab3/src/lab3.c
@@ -30,9 +30,20 @@ const float_t twoPiF0 = M_PI * 2 * f0;
 const float_t t0twoPiF0 = t0 * twoPiF0;
 const float_t t0twoPi = t0 * 2 * M_PI;
 const uint32_t filler = 0xFFFFFFFF;
+float_t unborder_filler;
+float_t tauSqr;
+__m256 vec_tauSqr;
+__m256 vec_HXdeg2mul2;
+__m256 vec_HYdeg2mul2;
+__m256 vec_two;
 float_t* create(int ny, int nx);
 float_t* calc_P(int ny, int nx);
-inline void swap(float_t* a, float_t* b) {
+void compute_one_matrix_line(float_t* dst_U_matrix,
+                             const float_t* src_U_matrix,
+                             const float_t* P,
+                             const size_t line,
+                             const size_t NX);
+void swap(float_t* a, float_t* b) {
   float_t tmp = *a;
   *a = *b;
   *b = tmp;
@@ -44,7 +55,7 @@ float_t sqr(const float_t a);
  */
 /* (a, b, c, d, e, f, g, h)  (a2, b2, c2, d2, e2, f2, g2, h2)->
  * (h, a2, b2, c2, d2, e2, f2, g2) */
-inline __m256 get_left_vector(__m256 a, __m256 b) {
+__m256 get_left_vector(__m256 a, __m256 b) {
   float temp_vec[8];
   _mm256_store_ps(temp_vec, _mm256_permute_ps(_mm256_blend_ps(a, b, 0b01111111),
                                               0b10010011));
@@ -56,7 +67,7 @@ inline __m256 get_left_vector(__m256 a, __m256 b) {
  */
 /* (a, b, c, d, e, f, g, h)  (a2, b2, c2, d2, e2, f2, g2, h2)->
  * (b, c, d, e, f, g, a2) */
-inline __m256 get_right_vector(__m256 a, __m256 b) {
+__m256 get_right_vector(__m256 a, __m256 b) {
   float temp_vec[8];
   _mm256_store_ps(temp_vec, _mm256_permute_ps(_mm256_blend_ps(a, b, 0b00000001),
                                               0b00111001));
@@ -72,9 +83,10 @@ int main(int argc, const char* argv[]) {
   const uint64_t y_bord = NY - 1;
   const float_t HXdeg2mul2 = 1 / (2 * sqr((XB - XA) / (NX - 1)));
   const float_t HYdeg2mul2 = 1 / (2 * sqr((YB - YA) / (NY - 1)));
-  const __m256 vec_HXdeg2mul2 = _mm256_set1_ps(HXdeg2mul2);
-  const __m256 vec_HYdeg2mul2 = _mm256_set1_ps(HYdeg2mul2);
-  const __m256 vec_two = _mm256_set1_ps(2.0f);
+  vec_HXdeg2mul2 = _mm256_set1_ps(HXdeg2mul2);
+  vec_HYdeg2mul2 = _mm256_set1_ps(HYdeg2mul2);
+  vec_two = _mm256_set1_ps(2.0f);
+  unborder_filler = *(float*)&filler;
 
 #ifdef DEBUG
   FILE* max_u_file = fopen("max_u_file", "wb");
@@ -83,13 +95,11 @@ int main(int argc, const char* argv[]) {
   if (NX > 1000 || NY > 1000) {
     tau = 0.001;
   }
-
   const float_t tauSqr = sqr(tau);
-  const __m256 vec_tauSqr = _mm256_set1_ps(tauSqr);
+  vec_tauSqr = _mm256_set1_ps(tauSqr);
 
   float_t* U = create(NY, NX);
-  float_t* uMinusOne = create(NY, NX);
-  float_t* uPlusOne = create(NY, NX);
+  float_t* U_new_iter = create(NY, NX);
   float_t* P = calc_P(NY, NX);
 
 #ifdef DEBUG
@@ -101,115 +111,18 @@ int main(int argc, const char* argv[]) {
 #endif
   const float_t tautwoPiF0 = tau * t0twoPiF0;
   float_t arg = -tautwoPiF0 - t0twoPi;
-  const float_t unborder_filler = *(float_t*)&filler;
+  /*const float_t unborder_filler = *(float_t*)&filler;*/
   for (size_t n = 0; n < NT; n++) {
-    for (size_t i = 1; i < y_bord; i++) {
-      size_t cur_line = i * NX;
-
-      __m256* vec_U_cur = (__m256*)(U + cur_line);
-      __m256* vec_uPlusOne_cur = (__m256*)(uPlusOne + cur_line);
-      __m256* vec_uMinusOne_cur = (__m256*)(uMinusOne + cur_line);
-      __m256* vec_U_line_upper = (__m256*)(U + cur_line - NX);
-      __m256* vec_U_line_lower = (__m256*)(U + cur_line + NX);
-
-      __m256 vec_U_prev = _mm256_setzero_ps();
-      __m256 vec_U_mid = vec_U_cur[0];
-      __m256 vec_U_next = vec_U_cur[1];
-
-      __m256* vec_P_upper = (__m256*)(P + cur_line - NX);
-      __m256* vec_P_lower = (__m256*)(P + cur_line);
-      __m256 vec_P_upper_prev = _mm256_setzero_ps();
-      __m256 vec_P_lower_prev = _mm256_setzero_ps();
-
-      for (size_t j = 0; j < NX; j += SIZE_OF_VECTOR) {
-        __m256 P_01 = *vec_P_upper;                             // P[i-1][j]
-        __m256 P_11 = *vec_P_lower;                             // P[i][j]
-        __m256 P_00 = get_left_vector(vec_P_upper_prev, P_01);  // P[i-1][j-1]
-        __m256 P_10 = get_left_vector(vec_P_lower_prev, P_11);  // P[i][j]
-        float_t left_border_elem = 0.0f;
-        float_t right_border_elem = 0.0f;
-
-        if (j != 0) {
-          left_border_elem = unborder_filler;
-        }
-
-        if (j + SIZE_OF_VECTOR < NX) {
-          right_border_elem = unborder_filler;
-        }
-        __m256 border_mask =
-            _mm256_set_ps(left_border_elem, unborder_filler, unborder_filler,
-                          unborder_filler, unborder_filler, unborder_filler,
-                          unborder_filler, right_border_elem);
-
-        __m256 vec_U_left_val =
-            _mm256_and_ps(get_left_vector(vec_U_prev, vec_U_mid), border_mask);
-        __m256 vec_U_right_val =
-            _mm256_and_ps(get_right_vector(vec_U_mid, vec_U_next), border_mask);
-
-        __m256 vec_U1 = _mm256_sub_ps(*vec_U_line_upper, vec_U_mid);
-        __m256 vec_P1 = _mm256_add_ps(P_00, P_01);
-
-        __m256 vec_U2 = _mm256_sub_ps(*vec_U_line_lower, vec_U_mid);
-        __m256 vec_P2 = _mm256_add_ps(P_10, P_11);
-
-        __m256 vec_first =
-            _mm256_mul_ps((_mm256_add_ps(_mm256_mul_ps(vec_U1, vec_P1),
-                                         _mm256_mul_ps(vec_U2, vec_P2))),
-                          vec_HXdeg2mul2);
-
-        __m256 vec_U3 = _mm256_sub_ps(vec_U_right_val, vec_U_mid);
-        __m256 vec_P3 = _mm256_add_ps(P_01, P_11);
-
-        __m256 vec_U4 = _mm256_sub_ps(vec_U_left_val, vec_U_mid);
-        __m256 vec_P4 = _mm256_add_ps(P_00, P_10);
-
-        __m256 vec_second =
-            _mm256_mul_ps((_mm256_add_ps(_mm256_mul_ps(vec_U3, vec_P3),
-                                         _mm256_mul_ps(vec_U4, vec_P4))),
-                          vec_HYdeg2mul2);
-
-        *vec_uPlusOne_cur = _mm256_fmadd_ps(
-            vec_tauSqr, _mm256_add_ps(vec_first, vec_second),
-            _mm256_fmsub_ps(vec_two, vec_U_mid, *vec_uMinusOne_cur));
-
-#ifdef DEBUG
-        vec_temp_max = _mm256_max_ps(
-            vec_temp_max, _mm256_and_ps(*vec_uPlusOne_cur, abs_mask));
-#endif
-        ++vec_U_cur;
-        ++vec_uPlusOne_cur;
-        ++vec_uMinusOne_cur;
-        ++vec_U_line_lower;
-        ++vec_U_line_upper;
-
-        vec_U_prev = vec_U_mid;
-        vec_U_mid = vec_U_next;
-        vec_U_next = vec_U_cur[1];
-
-        ++vec_P_upper;
-        ++vec_P_lower;
-        vec_P_upper_prev = P_01;
-        vec_P_lower_prev = P_11;
-      }
+    for (size_t line = 1; line < y_bord; ++line) {
+      compute_one_matrix_line(U_new_iter, U, P, line, NX);
     }
-
     arg += tautwoPiF0;
-    uPlusOne[SX + SY * (NX)] += tauSqr * expf(-sqr(arg * gamma)) * sinf(arg);
-
-    float_t* t = uMinusOne;
-    uMinusOne = U;
-    U = uPlusOne;
-    uPlusOne = t;
-#ifdef DEBUG
-    _mm256_store_ps(unpacked_max_vec, vec_temp_max);
-    for (size_t i = 0; i < SIZE_OF_VECTOR; ++i) {
-      if (temp_max < unpacked_max_vec[i]) {
-        temp_max = unpacked_max_vec[i];
-      }
-    }
-    fprintf(max_u_file, "\nStep:%zu\\%u Max: %f", n, NT, temp_max);
-#endif
+    U_new_iter[SX + SY * (NX)] += tauSqr * expf(-sqr(arg * gamma)) * sinf(arg);
+    float_t* tmp_matrix = U;
+    U = U_new_iter;
+    U_new_iter = tmp_matrix;
   }
+
 #ifdef DEBUG
   FILE* output = fopen(OUTPUT_FILE, "wb");
   if (output == NULL) {
@@ -218,7 +131,7 @@ int main(int argc, const char* argv[]) {
   }
 
   for (int i = 0; i < NY; ++i) {
-    fwrite(U + i * NX, sizeof(float_t), NX, output);
+    fwrite(U_new_iter + i * NX, sizeof(float_t), NX, output);
   }
   fclose(output);
 #endif
@@ -252,4 +165,85 @@ float_t* calc_P(int ny, int nx) {
     }
   }
   return matrix;
+}
+
+void compute_one_matrix_line(float_t* dst_U_matrix,
+                             const float_t* src_U_matrix,
+                             const float_t* P,
+                             const size_t line,
+                             const size_t NX) {
+  __m256* U_cur = (__m256*)(src_U_matrix + line * NX);
+  __m256* U_dst = (__m256*)(dst_U_matrix + line * NX);
+
+  __m256* vec_U_upper = (__m256*)(src_U_matrix + line * NX - NX);
+  __m256* vec_U_lower = (__m256*)(src_U_matrix + line * NX + NX);
+
+  __m256 vec_U_prev = _mm256_setzero_ps();
+  __m256 vec_U_mid = U_cur[0];
+  __m256 vec_U_next = U_cur[1];
+
+  __m256* vec_P_upper = (__m256*)(P + line * NX - NX);
+  __m256* vec_P_lower = (__m256*)(P + line * NX);
+  __m256 vec_P_upper_prev = _mm256_setzero_ps();
+  __m256 vec_P_lower_prev = _mm256_setzero_ps();
+
+  for (size_t j = 0; j < NX; j += SIZE_OF_VECTOR) {
+    __m256 P_01 = *vec_P_upper;                             // P[i-1][j]
+    __m256 P_11 = *vec_P_lower;                             // P[i][j]
+    __m256 P_00 = get_left_vector(vec_P_upper_prev, P_01);  // P[i-1][j-1]
+    __m256 P_10 = get_left_vector(vec_P_lower_prev, P_11);  // P[i][j-1]
+    float_t left_border_elem = 0.0f;
+    float_t right_border_elem = 0.0f;
+
+    if (j != 0) {
+      left_border_elem = unborder_filler;
+    }
+
+    if (j + SIZE_OF_VECTOR < NX) {
+      right_border_elem = unborder_filler;
+    }
+    __m256 border_mask = _mm256_set_ps(
+        left_border_elem, unborder_filler, unborder_filler, unborder_filler,
+        unborder_filler, unborder_filler, unborder_filler, right_border_elem);
+
+    __m256 vec_U_left_val =
+        _mm256_and_ps(get_left_vector(vec_U_prev, vec_U_mid), border_mask);
+    __m256 vec_U_right_val =
+        _mm256_and_ps(get_right_vector(vec_U_mid, vec_U_next), border_mask);
+
+    __m256 vec_U1 = _mm256_sub_ps(*vec_U_upper, vec_U_mid);
+    __m256 vec_P1 = _mm256_add_ps(P_00, P_01);
+
+    __m256 vec_U2 = _mm256_sub_ps(*vec_U_lower, vec_U_mid);
+    __m256 vec_P2 = _mm256_add_ps(P_10, P_11);
+
+    __m256 vec_first = _mm256_mul_ps(
+        (_mm256_fmadd_ps(vec_U1, vec_P1, _mm256_mul_ps(vec_U2, vec_P2))),
+        vec_HXdeg2mul2);
+
+    __m256 vec_U3 = _mm256_sub_ps(vec_U_right_val, vec_U_mid);
+    __m256 vec_P3 = _mm256_add_ps(P_01, P_11);
+
+    __m256 vec_U4 = _mm256_sub_ps(vec_U_left_val, vec_U_mid);
+    __m256 vec_P4 = _mm256_add_ps(P_00, P_10);
+
+    __m256 vec_second = _mm256_mul_ps(
+        (_mm256_fmadd_ps(vec_U3, vec_P3, _mm256_mul_ps(vec_U4, vec_P4))),
+        vec_HYdeg2mul2);
+    *U_dst = _mm256_fmadd_ps(vec_tauSqr, _mm256_add_ps(vec_first, vec_second),
+                             _mm256_fmsub_ps(vec_two, vec_U_mid, *U_dst));
+    ++U_cur;
+    ++vec_U_upper;
+    ++vec_U_lower;
+    ++U_dst;
+
+    vec_U_prev = vec_U_mid;
+    vec_U_mid = vec_U_next;
+    vec_U_next = *(U_cur + 1);
+
+    ++vec_P_upper;
+    ++vec_P_lower;
+    vec_P_upper_prev = P_01;
+    vec_P_lower_prev = P_11;
+  }
 }
