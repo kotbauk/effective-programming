f9ce502f730ecb2c307cee4d269afdba0205185f
Zykin Mikhail <kotbauk@gmail.com>
2020-06-15 08:56:09 +0700

    Lab2 handle variant

---
+++ my_labs/lab2/run.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+EXE=lab2
+RESULTS_DIR=results
+NUMBER_X_POINT=100
+NUMBER_Y_POINT=100
+STEPS=5000
+OPT_LEVEL="-O3"
+PERF_EVENT="branch-misses,L1-dcache-load-misses,LLC-load-misses,cycles,instructions,cache-misses"
+PROFILE_RUN=false
+function print_input() {
+echo "Grid: ${NUMBER_X_POINT}x${NUMBER_Y_POINT} Steps:$STEPS"
+}
+function plot_picture() {
+	gnuplot plot_test_png.p 
+}
+
+make OPT_LEVEL="$OPT_LEVEL" DEBUG_MODE="-DDEBUG_MODE" rebuild
+if [ ! -d $RESULTS_DIR ]; then
+	mkdir $RESULTS_DIR
+fi	
+cd $RESULTS_DIR 
+echo "Test run"
+print_input
+../$EXE $NUMBER_X_POINT $NUMBER_Y_POINT $STEPS 
+echo "Test picture ploting"
+plot_picture
+
+cd -
+make OPT_LEVEL="$OPT_LEVEL" rebuild
+cd $RESULTS_DIR
+
+echo "Performance run"
+NUMBER_X_POINT=5000
+NUMBER_Y_POINT=5000
+STEPS=100
+print_input
+time ../$EXE $NUMBER_X_POINT $NUMBER_Y_POINT $STEPS 
+PROFILE_RUN="false"
+if [[ $PROFILE_RUN == true ]]; then
+	NUMBER_X_POINT=5000
+	NUMBER_Y_POINT=5000
+	STEPS=100
+	cd -
+	make OPT_LEVEL="$OPT_LEVEL" PROFILE_OPTIONS="-g -pg" rebuild
+	cd $RESULTS_DIR
+	echo "Gprof run"
+	echo "Grid: ${NUMBER_X_POINT}x${NUMBER_Y_POINT} Steps:$STEPS"
+	../$EXE $NUMBER_X_POINT $NUMBER_Y_POINT $STEPS 
+	gprof ../$EXE > ${EXE}.profile.txt
+	gprof ../$EXE | gprof2dot -f prof -n0 -e0 | \
+	dot -T png  -o callgraph.png
+	gprof -A ../$EXE > ${EXE}.source.txt
+
+	cd -
+	make OPT_LEVEL="$OPT_LEVEL" PROFILE_OPTIONS="-ggdb" rebuild
+	cd $RESULTS_DIR
+	NUMBER_X_POINT=5000
+	NUMBER_Y_POINT=5000
+	STEPS=100
+	echo "Perf run: collecting $PERF_EVENT"
+	echo "Grid: ${NUMBER_X_POINT}x${NUMBER_Y_POINT} Steps:$STEPS"
+	perf record -e $PERF_EVENT ../$EXE $NUMBER_X_POINT $NUMBER_Y_POINT $STEPS 
+	perf stat -d -d -d -B -e $PERF_EVENT ../$EXE $NUMBER_X_POINT $NUMBER_Y_POINT $STEPS
+fi
+
+
--- my_labs/lab2/src/main.c
+++ my_labs/lab2/src/main.c
@@ -13,8 +13,6 @@
 #define BEGIN_Y 0.0f
 #define END_Y 4.0f
 #define VEC_TYPE_ALIGN 32
-const uint64_t filler = 0xFFFFFFFFFFFFFFFF;
-my_float_t unborder_filler;
 typedef __m256d my_vec_t;
 #define SCAL_IN_VEC sizeof(my_vec_t) / sizeof(my_float_t)
 typedef struct modeling_area {
@@ -47,12 +45,14 @@ my_matrix_t* compute_results(my_matrix_t* results_matrix,
                              const size_t number_of_steps,
                              const modeling_area mod_area);
 /* (a, b, c, d); (e, f, g, h) -> (d, e, f, g) */
-my_vec_t get_left_vector(my_vec_t a, my_vec_t b) {
+inline my_vec_t __attribute__((__always_inline__))
+get_left_vector(my_vec_t a, my_vec_t b) {
   return _mm256_permute4x64_pd(_mm256_blend_pd((a), (b), 0x07), 0b10010011);
 }
 
 /* (a, b, c, d); (e, f, g, h) -> (b, c, d, e) */
-my_vec_t get_right_vector(my_vec_t a, my_vec_t b) {
+inline my_vec_t __attribute__((__always_inline__))
+get_right_vector(my_vec_t a, my_vec_t b) {
   return _mm256_permute4x64_pd(_mm256_blend_pd((a), (b), 0x01), 0b111001);
 }
 int main(int argc, const char* argv[]) {
@@ -115,13 +115,13 @@ modeling_area init_modeling_area(const my_float_t begin_x,
                             .hy = (end_y - begin_y) / (number_y_points - 1)};
   return mod_area;
 }
-my_float_t compute_po_in_point(my_float_t xj,
-                               my_float_t yi,
-                               my_float_t xs1,
-                               my_float_t xs2,
-                               my_float_t ys1,
-                               my_float_t ys2,
-                               my_float_t sqr_r) {
+inline my_float_t compute_po_in_point(my_float_t xj,
+                                      my_float_t yi,
+                                      my_float_t xs1,
+                                      my_float_t xs2,
+                                      my_float_t ys1,
+                                      my_float_t ys2,
+                                      my_float_t sqr_r) {
   if (pow((xj - xs1), 2) + pow((yi - ys1), 2) < sqr_r) {
     return 0.1;
   } else if (pow((xj - xs2), 2) + pow((yi - ys2), 2) < sqr_r) {
@@ -165,24 +165,20 @@ my_matrix_t* compute_results(my_matrix_t* results_matrix,
   const my_float_t sqr_hx = mod_area.hx * mod_area.hx;
   const my_float_t sqr_hy = mod_area.hy * mod_area.hy;
   const my_float_t sum_of_sqr_h = 1 / sqr_hx + 1 / sqr_hy;
-  const my_vec_t vec_sqr_hx = _mm256_set1_pd(sqr_hx);
-  const my_vec_t vec_sqr_hy = _mm256_set1_pd(sqr_hy);
-  const my_vec_t vec_sum_of_sqr_h = _mm256_set1_pd(1 / sqr_hx + 1 / sqr_hy);
   const my_vec_t vec_first_multiplier = _mm256_set1_pd(0.2 / sum_of_sqr_h);
   const my_vec_t vec_second_multiplier =
       _mm256_set1_pd(2.5 / sqr_hx - 0.5 / sqr_hy);
   const my_vec_t vec_third_multiplier =
       _mm256_set1_pd(2.5 / sqr_hy - 0.5 / sqr_hx);
-  const my_vec_t vec_fourth_multiplier = _mm256_set1_pd(0.25 * (sum_of_sqr_h));
+  const my_vec_t vec_fourth_multiplier = _mm256_set1_pd(0.25 * sum_of_sqr_h);
   const my_vec_t vec_two = _mm256_set1_pd(2.f);
   const my_vec_t vec_quarter = _mm256_set1_pd(0.25f);
 #ifdef DEBUG_MODE
   my_float_t max_delta = .0;
   my_float_t temp_delta = .0;
 #endif
-  const my_float_t border_filler = *(my_float_t*)&filler;
   for (size_t n = 0; n < number_of_steps; ++n) {
-    for (size_t i = 1; i < mod_area.max_i + 1; ++i) {
+    for (size_t i = 1; i < mod_area.number_y_points - 1; ++i) {
       my_vec_t* temp_matrix_cur = (my_vec_t*)(get_matrix_buffer(temp_matrix) +
                                               i * mod_area.number_x_points);
       my_vec_t* results_matrix_up =
@@ -222,53 +218,20 @@ my_matrix_t* compute_results(my_matrix_t* results_matrix,
       my_vec_t vec_po_low = po_low[0];
 
       for (size_t j = 0; j < mod_area.number_x_points; j += SCAL_IN_VEC) {
-        my_float_t left_border_flag = 0.0;
-        my_float_t right_border_flag = 0.0;
-        /*if (j != 0) {*/
-          /*left_border_flag = border_filler;*/
-        /*}*/
-        /*if (j < mod_area.number_x_points) {*/
-          /*right_border_flag = border_filler;*/
-        /*}*/
-        /*__m256d border_mask = _mm256_set_pd(right_border_flag, border_filler,*/
-                                            /*border_filler, left_border_flag);*/
-        /*my_vec_t vec_po_left = _mm256_and_pd(*/
-            /*get_left_vector(vec_po_prev, vec_po_cur), border_mask);*/
-        /*my_vec_t vec_po_right = _mm256_and_pd(*/
-            /*get_right_vector(vec_po_cur, vec_po_next), border_mask);*/
+        my_vec_t vec_po_left = get_left_vector(vec_po_prev, vec_po_cur);
+        my_vec_t vec_po_right = get_right_vector(vec_po_cur, vec_po_next);
 
-        /*my_vec_t vec_res_left = _mm256_and_pd(*/
-            /*get_left_vector(vec_res_prev, vec_res_cur), border_mask);*/
-        /*my_vec_t vec_res_right = _mm256_and_pd(*/
-            /*get_right_vector(vec_res_cur, vec_res_next), border_mask);*/
-
-        /*my_vec_t vec_res_up_left = _mm256_and_pd(*/
-            /*get_left_vector(vec_res_up_prev, vec_res_up_cur), border_mask);*/
-        /*my_vec_t vec_res_up_right = _mm256_and_pd(*/
-            /*get_right_vector(vec_res_up_cur, vec_res_up_next), border_mask);*/
-
-        /*my_vec_t vec_res_low_left = _mm256_and_pd(*/
-            /*get_left_vector(vec_res_low_prev, vec_res_low_cur), border_mask);*/
-        /*my_vec_t vec_res_low_right = _mm256_and_pd(*/
-            /*get_right_vector(vec_res_low_cur, vec_res_low_next), border_mask);*/
-        my_vec_t vec_po_left = 
-            get_left_vector(vec_po_prev, vec_po_cur);
-        my_vec_t vec_po_right = 
-            get_right_vector(vec_po_cur, vec_po_next);
-
-        my_vec_t vec_res_left = 
-            get_left_vector(vec_res_prev, vec_res_cur);
-        my_vec_t vec_res_right =
-            get_right_vector(vec_res_cur, vec_res_next);
+        my_vec_t vec_res_left = get_left_vector(vec_res_prev, vec_res_cur);
+        my_vec_t vec_res_right = get_right_vector(vec_res_cur, vec_res_next);
 
         my_vec_t vec_res_up_left =
             get_left_vector(vec_res_up_prev, vec_res_up_cur);
-        my_vec_t vec_res_up_right = 
+        my_vec_t vec_res_up_right =
             get_right_vector(vec_res_up_cur, vec_res_up_next);
 
-        my_vec_t vec_res_low_left = 
+        my_vec_t vec_res_low_left =
             get_left_vector(vec_res_low_prev, vec_res_low_cur);
-        my_vec_t vec_res_low_right = 
+        my_vec_t vec_res_low_right =
             get_right_vector(vec_res_low_cur, vec_res_low_next);
 
         my_vec_t first_bracket = _mm256_mul_pd(
@@ -287,7 +250,7 @@ my_matrix_t* compute_results(my_matrix_t* results_matrix,
             vec_two, vec_po_cur,
             _mm256_mul_pd(
                 vec_quarter,
-                _mm256_add_pd(_mm256_add_pd(*po_low,*po_up),
+                _mm256_add_pd(_mm256_add_pd(vec_po_low, vec_po_up),
                               _mm256_add_pd(vec_po_left, vec_po_right))));
         my_vec_t fourth_bracket =
             _mm256_add_pd(_mm256_add_pd(first_bracket, second_bracket),
